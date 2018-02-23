593ad7cb7953f2b7f6fd39f2e36e7fd2d00a1ac9
Владислав Гнилозуб <vlad.gnilozyb@gmail.com>
2018-02-23 17:22:35 +0300

    writing logic for correctly display in collectionVIew images from Firebase also added view for cell of collectionVIew

--- MemeCreator/MemeCreator/Controllers/ImageCollectionViewController.swift
+++ MemeCreator/MemeCreator/Controllers/ImageCollectionViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import Firebase
 
+
 class ImageCollectionViewController: UIViewController {
     private var memes = [Meme]()
     @IBOutlet weak var imageCollection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
     }
     
     func fetchImage() {
@@ -28,6 +30,7 @@ class ImageCollectionViewController: UIViewController {
                         DispatchQueue.main.async(execute: {
                             let originalImage = Meme(originalImage: UIImage(data: data!)!)
                             self.memes.insert(originalImage, at: 0)
+                            self.imageCollection.reloadData()
                         })
                     }
                 })
@@ -35,6 +38,23 @@ class ImageCollectionViewController: UIViewController {
         })
     }
 
+}
 
+extension ImageCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.imageInCell.image = self.memes[indexPath.item].originalImage
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let createMemeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CreateMemeViewController") as! CreateMemeViewController
+        present(createMemeVC, animated: true) {
+            createMemeVC.imageView.image = self.memes[indexPath.item].originalImage.compressImage()
+        }
+    }
 }
---
+++ MemeCreator/MemeCreator/VIew/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  MemeCreator
+//
+//  Created by Владислав Гнилозуб on 2/23/18.
+//  Copyright © 2018 Владислав Гнилозуб. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageInCell: UIImageView!
+}
