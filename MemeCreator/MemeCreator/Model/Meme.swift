42f686e1d54b67c409d96b529ada5f0ad03a972c
Владислав Гнилозуб <vlad.gnilozyb@gmail.com>
2018-02-23 17:00:54 +0300

    create new model named Meme this model describe Meme

---
+++ MemeCreator/MemeCreator/Model/Meme.swift
@@ -0,0 +1,19 @@
+//
+//  Meme.swift
+//  MemeCreator
+//
+//  Created by Владислав Гнилозуб on 2/19/18.
+//  Copyright © 2018 Владислав Гнилозуб. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String?
+    let buttonText: String?
+    let originalImage: UIImage
+    let memedImage: UIImage?
+
+    
+}
