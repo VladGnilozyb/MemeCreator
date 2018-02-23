c039a381360b52c2e5195f2a8e69cf48aeac01d3
Владислав Гнилозуб <vlad.gnilozyb@gmail.com>
2018-02-23 16:52:42 +0300

    create new file CreateMemeViewController

---
+++ MemeCreator/MemeCreator/Controllers/CreateMemeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateMemeViewController.swift
+//  MemeCreator
+//
+//  Created by Владислав Гнилозуб on 2/23/18.
+//  Copyright © 2018 Владислав Гнилозуб. All rights reserved.
+//
+
+import UIKit
+
+class CreateMemeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
